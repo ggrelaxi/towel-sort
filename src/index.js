d6ee4d08484f0ea42a738f4db80de7d8aac33dc1
Alex Smirnov <ggrelaxi@gmail.com>
2023-01-31 18:36:17 +0300

    finish towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) return [];
+    return matrix.reduce((acc, item, index) => {
+        if (index === 0 || index % 2 === 0) {
+            acc.push(...item);
+        } else {
+            acc.push(...item.reverse());
+        }
+        return acc;
+    }, []);
+};
